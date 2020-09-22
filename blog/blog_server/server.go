a4656e3624f684baeca4c4efd269c62127407927
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-09-22 18:18:17 +0300

    Blog Creating

---
+++ blog/blog_server/server.go
@@ -0,0 +1,40 @@
+package blog_server
+
+import (
+	"github.com/Anatol-e/grpcapp/blog/blogpb"
+	"github.com/Anatol-e/grpcapp/greet/greetpb"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+type server {
+
+}
+
+func main() {
+	// SSL
+	certFile := "ssl/server.crt"
+	keyFile := "ssl/server.pem"
+	creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if sslErr != nil {
+		log.Fatalf("Failing loading certificates : %v", sslErr)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+
+	log.Info("Listening...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc sumtaskapp/sumpb/sum.proto --go_out=plugins=grpc:.
+protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
