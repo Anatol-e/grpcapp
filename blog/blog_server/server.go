72388afdaca9788b480e11aa5f5534d138a62516
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-09-22 22:31:18 +0300

    Mongo testing

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,18 +1,58 @@
-package blog_server
+package main
 
 import (
+	"context"
 	"github.com/Anatol-e/grpcapp/blog/blogpb"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"net"
+	"time"
 )
+
+type Blog struct {
+	Id   int64  `json:"id"`
+	Text string `json:"text"`
+}
 
 type server struct {
 }
 
+func (s *server) CreateBlog(ctx context.Context, request *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+	panic("implement me")
+}
+
 func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err != nil {
+		log.Fatalf("Cant connect to the mongo with an err=%v", err)
+	}
+	err = client.Connect(ctx)
+
+	blog := &Blog{
+		Id:   1,
+		Text: "Blog text Blog text Blog text Blog text ",
+	}
+	collection := client.Database("mg_db_rpc").Collection("blog")
+	_, err = collection.InsertOne(ctx, blog)
+	if err != nil {
+		log.Fatalf("Can't insert row %v", err)
+	}
+
+	result, err := collection.Find(ctx, bson.D{{"id",1}})
+	if err != nil {
+		log.Fatalf("Can't find data %v", err)
+	}
+	result.Decode(&blog)
+	log.Infof("coll %v", *blog)
+
 	// SSL
 	certFile := "ssl/server.crt"
 	keyFile := "ssl/server.pem"
--- go.mod
+++ go.mod
@@ -5,14 +5,12 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/google/go-cmp v0.5.2 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.5.1 // indirect
+	go.mongodb.org/mongo-driver v1.4.1
 	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
 	golang.org/x/sys v0.0.0-20200428200454-593003d681fa // indirect
-	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/grpc v1.31.1
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
